b400e5f79a7cf5d704858253ab29ee4531f9932a
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2024-04-18 07:10:38 +0700

    init section11

---
+++ section11/cards/src/main/java/com/nnt/cards/entity/Cards.java
@@ -0,0 +1,26 @@
+package com.nnt.cards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@ToString
+public class Cards extends BaseEntiry{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long cardId;
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private int totalLimit;
+    private int amountUsed;
+    private int availableAmount;
+
+}
