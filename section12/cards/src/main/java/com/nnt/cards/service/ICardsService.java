9ae591fa6ce350b6dd271cf2301b30339f1005f4
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2024-04-20 22:06:52 +0700

    init section12

---
+++ section12/cards/src/main/java/com/nnt/cards/service/ICardsService.java
@@ -0,0 +1,14 @@
+package com.nnt.cards.service;
+
+import com.nnt.cards.dto.CardsDto;
+
+public interface ICardsService {
+
+    void createCard(String mobileNumber);
+
+    CardsDto fetchCard(String mobileNumber);
+
+    boolean updateCard(CardsDto cardsDto);
+
+    boolean deleteCard(String mobileNumber);
+}
